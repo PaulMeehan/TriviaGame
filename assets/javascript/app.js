b27caffcc5a85bc55a20d86423107939ea6ba801
PaulM <pmeehan456@gmail.com>
2019-01-12 09:58:48 -0500

    timer working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,61 +1,132 @@
 
-var trivia = {
-    question: "Which soft drink was originally called Brad's Drink",
+var trivia = [
+{
+    question: "Which soft drink was originally called Brad's Drink?",
     answers: ["Coca-Cola", "Mountain Dew", "Pepsi", "Dr Pepper"],
     correctAnswer: 2,
     image: "pepsi.jpg"
+},
+{   question: "Who is the most wonderful wife in the world?",
+    answers: ["Beverly Goldberg", "My Sweetheart!", "Mary Poppins", "June Cleaver"],
+    correctAnswer: 1,
+    image: "cheryl.jpg"
+}
+];
+
+var questionNumber = 0;
+var time = 0;
+var intervalID;
+var wins = 0;
+var losses = 0;
+var timeOuts = 0;
+
+function endOfGame () {
+    $("#graphics").empty();
+    $("#result").text("Game Over!");
+    $("#graphics").append("<p>Number of wins: " + wins + "</p>");
+    $("#graphics").append("<p>Number of losses: " + losses + "</p>");
+    $("#graphics").append("<p>Number of time outs: " + timeOuts + "</p>");
+    
 };
 
+function endOfQuestion (result) {
+    //stop the timer
+    clearTimeout(intervalID);
+    //display message according to the result
+    switch (result) {
+        case "NoTime":
+            $("#result").text("Time's Up!");
+            timeOuts = timeOuts +1;
+            break;
+        case "correct":
+            $("#result").text("Correct!");
+            wins = wins + 1;
+            break;
+        case "wrong":
+            $("#result").text("Wrong!");
+            losses = losses + 1;
+    }
+    // first, empty the contents of the graphics section
+    $("#graphics").empty();
+    $("#graphics").append("<p>The correct answer was: " + trivia[questionNumber].answers[trivia[questionNumber].correctAnswer] + "</p>");
+    $("#graphics").append("<img src='assets/images/" + trivia[questionNumber].image + "' height :250px; width: 200px>");
+
+    //pause for 5 seconds and then display next question
+    intervalID = setTimeout(showQuestion, 5000);
+    
+    questionNumber = questionNumber + 1;
+    if (questionNumber >= trivia.length) {
+        endOfGame();
+    };
+
+
+};
+
+function decrement () {
+    //reduce the time variable by 1
+    time = time - 1;
+    //determine whether time is up
+    $("#timer").text(time);
+    if (time < 1) {
+        endOfQuestion("NoTime");
+    } 
+};
 
 function startGame () {
     console.log("startGame");
+    //show or hide the appropriate parts of the window
     $("#startup").show();
     $("#timerSection").hide();
-    $("#questionResult").hide();
+    $("#question").hide();
+    $("#result").hide();
     $("#graphics").hide();
     $("#restart").hide();
 };
 
 function checkAnswer (answer) {
+    //first stop the timer
+    clearTimeout(intervalID);
+    //clear out any existing elements
     $("#graphics").empty();
-    if (answer === trivia.correctAnswer) {
-        $("#questionResult").text("Correct!");
+    //determine whether answer was correct and display results
+    if (answer === trivia[questionNumber].correctAnswer) {
+        endOfQuestion("correct");
     } else {
-        $("#questionResult").text("Wrong!");
-        $("#graphics").append("<p>The correct answer was: " + trivia.answers[trivia.correctAnswer] + "</p>");
+        endOfQuestion("wrong");
     };
-    $("#graphics").append("<img src='assets/images/" + trivia.image + "'>");
 };
 
 function listQuestions () {
     // first, empty the contents of the graphics section
     $("#graphics").empty();
-    // now, create a new unordered list
-    // var qlist = $("<ul>");
+    // now, create a button for each possible answer.
     for (var i = 0; i <= 3; i++) {
-        // qlist.append("<li>" + trivia.answers[i] + "</li>");
         var newButton = $("<button>");
-        newButton.text(trivia.answers[i]);
+        newButton.text(trivia[questionNumber].answers[i]);
         newButton.attr("button-value", i);
         newButton.attr("onClick", "checkAnswer(" + i + ")");
         newButton.attr("class","answerButton");
         newButton.css("width","400px");
         $("#graphics").append(newButton);
     };
-    // qlist.append("</ul>");
 };
 
 function showQuestion () {
-    console.log("showQuestion");
+    //initiate the timer
+    time = 30;
+
     $("#startup").hide();
     $("#timerSection").show();
-    $("#timer").text("30");
-    $("#questionResult").show();
-    $("#questionResult").text(trivia.question);
+    $("#timer").text(time);
+    $("#question").show();
+    $("#question").text(trivia[questionNumber].question);
+    $("#result").show();
+    $("#result").text("");
     $("#graphics").show();
     listQuestions();
     $("#restart").hide();
-
+    //start the timer
+    intervalID = setInterval(decrement, 1000);
 };
 
 
