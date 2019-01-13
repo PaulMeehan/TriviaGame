dfdfb7b312b8d88395bef4952a0157df33b3e493
PaulM <pmeehan456@gmail.com>
2019-01-12 21:02:46 -0500

    questions randomly selected

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,46 @@
 
 var trivia = [
 // This object contains the following fields:
-// question = the question being asked
-// answers = an array of 4 possible answers, includes the correct answer
-// correctAnswer = the index of the array element that is the correct answer
-// image = the name of the image file to be displayed along with this question.
+//    question = the question being asked
+//    answers = an array of 4 possible answers, includes the correct answer
+//    correctAnswer = the index of the array element that is the correct answer
+//    image = the name of the image file to be displayed along with this question.
 {
     question: "Which soft drink was originally called Brad's Drink?",
     answers: ["Coca-Cola", "Mountain Dew", "Pepsi", "Dr Pepper"],
     correctAnswer: 2,
     image: "pepsi.jpg"
 },
-{   question: "Who is the most wonderful wife in the world?",
-    answers: ["Beverly Goldberg", "My Sweetheart!", "Mary Poppins", "June Cleaver"],
+{   question: "What is the longest line of constant latitude of the Earth?",
+    answers: ["Prime Meridian", "Tropic of Cancer", "Equator", "Arctic Circle"],
+    correctAnswer: 2,
+    image: "globe.jpg"
+},
+{   question: "Croissant is a type of pastry.  In French, what does the word 'croissant' mean?",
+    answers: ["Cramp", "Crust", "Crimson", "Crescent"],
+    correctAnswer: 3,
+    image: "croissant.jpg"
+},
+{   question: "In the United States, what is the name given to a presidnt who never served in Congress?",
+    answers: ["Washington outsider", "Jefferson outcast", "Arthur nomad", "Lame duck"],
+    correctAnswer: 0,
+    image: "washington.jpg"
+},
+{   question: "Which famous American university is part of Colonial Williamsburg?",
+    answers: ["Williams","William and Mary", "Kings College", "Columbia"],
     correctAnswer: 1,
-    image: "cheryl.jpg"
+    image: "wande.jpg"
 }
 ];
 
 // declare variables
-var questionNumber = 0;
-var time = 0;
-var intervalID;
-var wins = 0;
-var losses = 0;
-var timeOuts = 0;
+var currentQuestionNumber = 0;  // index of current question
+var time = 0;                   // number of seconds left to answer current questions
+var intervalID;                 // interval id
+var wins = 0;                   // number of questions answered correctly
+var losses = 0;                 // number of questions answered incorrectly
+var timeOuts = 0;               // number of questions where time ran out
+var questionNumbers = [];       // array of trivia index numbers to select from
 
 
 function endOfGame () {
@@ -37,8 +53,7 @@ function endOfGame () {
 };
 
 function checkForLastQuestion () {
-    questionNumber = questionNumber + 1;
-    if (questionNumber >= trivia.length) {
+    if (questionNumbers.length === 0) {
         endOfGame();
     } else {
         showQuestion();
@@ -64,8 +79,8 @@ function endOfQuestion (result) {
     }
     // first, empty the contents of the graphics section
     $("#graphics").empty();
-    $("#graphics").append("<p>The correct answer was: " + trivia[questionNumber].answers[trivia[questionNumber].correctAnswer] + "</p>");
-    $("#graphics").append("<img src='assets/images/" + trivia[questionNumber].image + "' height :250px; width: 200px>");
+    $("#graphics").append("<p>The correct answer was: " + trivia[currentQuestionNumber].answers[trivia[currentQuestionNumber].correctAnswer] + "</p>");
+    $("#graphics").append("<img src='assets/images/" + trivia[currentQuestionNumber].image + "' height :250px; width: 200px>");
 
     //pause for 5 seconds and then display next question
     intervalID = setTimeout(checkForLastQuestion, 5000);
@@ -89,7 +104,7 @@ function checkAnswer (answer) {
     //clear out any existing elements
     $("#graphics").empty();
     //determine whether answer was correct and display results
-    if (answer === trivia[questionNumber].correctAnswer) {
+    if (answer === trivia[currentQuestionNumber].correctAnswer) {
         endOfQuestion("correct");
     } else {
         endOfQuestion("wrong");
@@ -102,7 +117,7 @@ function listQuestions () {
     // now, create a button for each possible answer.
     for (var i = 0; i <= 3; i++) {
         var newButton = $("<button>");
-        newButton.text(trivia[questionNumber].answers[i]);
+        newButton.text(trivia[currentQuestionNumber].answers[i]);
         newButton.attr("button-value", i);
         newButton.attr("onClick", "checkAnswer(" + i + ")");
         newButton.attr("class","answerButton");
@@ -115,11 +130,17 @@ function showQuestion () {
     //initiate the timer
     time = 30;
 
+    // randomly select next question number
+    var randomNum = Math.floor(Math.random() * questionNumbers.length);
+    currentQuestionNumber = questionNumbers[randomNum];
+    // remove that value from the question numbers array
+    questionNumbers.splice(randomNum, 1);
+
     $("#startup").hide();
     $("#timerSection").show();
     $("#timer").text(time);
     $("#question").show();
-    $("#question").text(trivia[questionNumber].question);
+    $("#question").text(trivia[currentQuestionNumber].question);
     $("#result").show();
     $("#result").text("");
     $("#graphics").show();
@@ -129,13 +150,16 @@ function showQuestion () {
     intervalID = setInterval(decrement, 1000);
 };
 
-
 function startGame() {
     console.log("startGame");
-    //reinitialize counters    
-    questionNumber = 0;
+    // fill question numbers array with an index number for each element of trivia object
+    questionNumbers = [];
+    for (i=0; i < trivia.length; i++) {
+        questionNumbers.push(i);
+    }
+
+    //reset the counters    
     time = 0;
-    intervalID;
     wins = 0;
     losses = 0;
     timeOuts = 0;
